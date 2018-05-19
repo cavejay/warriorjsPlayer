d25ccd3b6984999b89b5afe23b060e3086445e7c
cavejay <michael@sablecliff.com>
2018-05-19 22:49:19 +1000

    many changes

--- wordman-beginner/Player.js
+++ wordman-beginner/Player.js
@@ -1,58 +1,97 @@
 class Player {
   constructor() {
+    // last health
     this._health = 20;
+
+    // last chosen direction
     this._direction = "backward";
+
+    // last action
+    this._action;
+
+    this.action; // current action in string form
+    this.chosenAction; // current action in clojure form
   }
 
   playTurn(warrior) {
-    // set a default chosen action
-    let chosenAction = undefined;
+    // store the warrior object for this turn
+    this.warrior = warrior;
 
+    // set a default chosen action
+    this.chosenAction = undefined;
+
+    // if our current direction is a wall, plz switch
     if (warrior.feel(this._direction).isWall()) {
-      this._direction = "forwards";
+      this._direction = "forward";
     }
 
     // Action Preference order
-    chosenAction =
-      chosenAction ||
-      this.actionHealIfSafe(warrior) ||
-      this.interaction(warrior) ||
-      (() => warrior.walk(this._direction));
+    this.chosenAction =
+      this.chosenAction || // keep the default
+      this.actionHealIfSafe() || // check for heals
+      this.interaction() || // check for enemy/captive
+      this.setAction("walk", this._direction); // walk in our current dir
 
-    // Do one action
-    chosenAction();
+    // Do the outcome of the above
+    this.chosenAction();
 
+    // store values for next time
     this._health = warrior.health();
+    this._action = this.action;
+  }
+
+  setAction(action, direction) {
+    this.action = action;
+    if (action == "rest") {
+      return this.warrior.rest;
+    }
+    return () => this.warrior[action](direction);
+  }
+
+  wasHurt() {
+    return this._health > this.warrior.health();
   }
 
   // Actioning feels
-  interaction(warrior) {
+  interaction() {
     // If there's nothing interact with then move on
-    if (warrior.feel().isEmpty()) {
+    if (this.warrior.feel(this._direction).isEmpty()) {
       return;
     }
 
     // if there is and it's a captive then rescue
-    if (warrior.feel().isCaptive()) {
-      return warrior.rescue;
+    if (this.warrior.feel(this._direction).isCaptive()) {
+      return this.setAction("rescue", this._direction);
     } else {
-      return warrior.attack;
+      return this.setAction("attack", this._direction);
     }
   }
 
   // Keeping us alive
-  actionHealIfSafe(warrior) {
+  actionHealIfSafe() {
     // If we're not hurt pass by
-    if (warrior.health() >= 20) {
+    if (this.warrior.health() >= 20) {
       return;
+    }
+
+    // if we healed last time but still lost health
+    // this check could be extended to deal with low health enemies
+    if (this._action == "rest" && this.wasHurt()) {
+      // move away from the direction we were going
+      // this.warrior.think("I should probably back up and heal");
+      if (this._direction == "backward") {
+        return this.setAction("walk", "forward");
+      } else {
+        return this.setAction("walk", "backward");
+      }
     }
 
     // If there's nothing infront of us that could attack
     // &
     // If we've not taken damage from last time
-    if (warrior.feel().isEmpty() && this._health <= warrior.health()) {
+    if (this.warrior.feel(this._direction).isEmpty() && !this.wasHurt()) {
       // rest up
-      return warrior.rest;
+      return this.setAction("rest");
     }
   }
 }
